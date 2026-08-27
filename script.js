4838e2d09747f6366524f705f8a7190788166732
Joao Gabriel <joaogabr53@gmail.com>
2026-08-26 23:26:07 -0300

    Primeiro commit - FitLife Academia

---
+++ script.js
@@ -0,0 +1,18 @@
+const elementos = document.querySelectorAll(".reveal");
+
+function revelarElementos() {
+    elementos.forEach((elemento) => {
+
+        const posicao = elemento.getBoundingClientRect().top;
+        const alturaTela = window.innerHeight;
+
+        if (posicao < alturaTela - 100) {
+            elemento.classList.add("active");
+        }
+
+    });
+}
+
+window.addEventListener("scroll", revelarElementos);
+
+revelarElementos();
