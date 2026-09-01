ffe00dd5c55e49cd06ae9f543d056b7a47ada756
João Gabriel <joaogabr53@gmail.com>
2026-08-31 21:32:13 -0300

    Atualiza site FitLife

--- script.js
+++ script.js
@@ -17,3 +17,42 @@ window.addEventListener("scroll", revelarElementos);
 
 revelarElementos();
 
+/* MENSAGEM PARA WHATSAPP */
+const formulario = document.getElementById("formContato");
+const mensagemErro = document.getElementById("mensagemErro");
+
+formulario.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const mensagem = document.getElementById("mensagem").value.trim();
+
+    if (nome === "" || email === "" || mensagem === "") {
+        mensagemErro.textContent = "Por favor, preencha todos os campos.";
+        return;
+    }
+
+    const formatoEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!formatoEmail.test(email)) {
+        mensagemErro.textContent = "Digite um e-mail válido.";
+        return;
+    }
+
+    mensagemErro.textContent = "";
+    
+    const texto = 
+        `Olá! Meu nome é ${nome}. ` +
+        `Meu e-mail é ${email}.%0A` +
+        `${mensagem}`;
+
+    const numero = "5581999659504";
+
+const url = `https://wa.me/${numero}?text=${texto}`;
+
+window.open(url, "_blank");
+
+
+});
